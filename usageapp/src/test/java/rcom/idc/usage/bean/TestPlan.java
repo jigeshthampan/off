a14df504ccaf39f8c08275a84f292ddc66c96623
Jigesh Thampan <Jigesh.Thampan@relianceada.com>
2017-09-13 12:20:26 +0530

    Test Added

--- usageapp/src/test/java/rcom/idc/usage/bean/TestPlan.java
+++ usageapp/src/test/java/rcom/idc/usage/bean/TestPlan.java
@@ -4,6 +4,8 @@ import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
+import rcom.idc.usage.helper.PlanType;
+
 
 public class TestPlan {
 	@Test
@@ -12,5 +14,12 @@ public class TestPlan {
 		plan.setId(1);
 		
 		assertEquals("Plan id is being set",1,plan.getId());
+	}
+	
+	@Test
+	public void planTypeTest() {
+		Plan plan = new DTPlan();
+		plan.setType(PlanType.STANDARD);
+		assertEquals("Plan Type is ",PlanType.STANDARD,plan.getType());
 	}
 }
